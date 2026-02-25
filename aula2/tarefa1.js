41bd2ccd4856f392adcfca343e606e0aefbc29c9
plim7plim <pliniobr33@gmail.com>
2026-02-25 20:24:39 -0300

    pqp

---
+++ aula2/tarefa1.js
@@ -0,0 +1,36 @@
+
+const variavel1 = 4;
+const variavel2 = 8;
+
+let soma = variavel1 + variavel2;
+console.log(soma);
+
+let subtracao = variavel1 - variavel2;
+console.log(subtracao);
+
+let multiplicacao = variavel1 * variavel2;
+console.log(multiplicacao);
+
+let divisao = variavel1 / variavel2;
+console.log(divisao);
+
+let divisao2 = variavel1 % variavel2;
+console.log(divisao2);
+
+let exponenciacao = variavel1 ** variavel2;
+console.log (exponenciacao);
+
+let quadrada = variavel1 ** (1/2);
+console.log(quadrada)
+
+let cubica = variavel2 ** (1/3);
+console.log(cubica)
+
+
+
+//exercicio 2
+
+const consumoVeiculo = 12.5;
+const precoGasolina = 6.20;
+let tempoViagem = 300;
+let velocidadeMedia = 40 ;
