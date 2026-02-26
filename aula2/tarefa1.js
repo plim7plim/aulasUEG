fc8bfcc0390425817cc158ce13faa86d6c569d4a
plim7plim <pliniobr33@gmail.com>
2026-02-25 21:37:27 -0300

    fim de aula

--- aula2/tarefa1.js
+++ aula2/tarefa1.js
@@ -1,4 +1,3 @@
-
 const variavel1 = 4;
 const variavel2 = 8;
 
@@ -21,16 +20,36 @@ let exponenciacao = variavel1 ** variavel2;
 console.log (exponenciacao);
 
 let quadrada = variavel1 ** (1/2);
-console.log(quadrada)
+console.log(quadrada);
 
 let cubica = variavel2 ** (1/3);
-console.log(cubica)
-
+console.log(cubica);
 
 
 //exercicio 2
 
-const consumoVeiculo = 12.5;
-const precoGasolina = 6.20;
-let tempoViagem = 300;
-let velocidadeMedia = 40 ;
+const kmPorLitro = 12.5;
+const custoGasolina = 6.20;
+
+const tempoViagem = 5;
+const velocidadeMedia = 80;
+
+let distanciaPercorrida =  tempoViagem * velocidadeMedia;
+console.log(distanciaPercorrida);
+
+let litros =  distanciaPercorrida / kmPorLitro;
+console.log(litros);
+
+let gasto = litros * custoGasolina;
+console.log("A distancia percorrida foi: " + distanciaPercorrida + " gastando um total de " + litros + " dando " + gasto + " reais" );
+
+
+//exercicio 3
+
+const coeficienteA= 2;
+const coeficienteB= 3;
+const coeficienteC= 5;
+
+
+const primeiraParte = coeficienteB ** 2 -4 * coeficienteA * coeficienteC
+console.log(primeiraParte);
