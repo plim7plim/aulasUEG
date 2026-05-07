f3b5f636600e1fc0ab63175ae1c3614169918d43
Plinio Peixoto dos Santos <pliniobr33@gmail.com>
2026-05-07 09:37:28 -0300

    add list6

--- aulasUEG2/tarefa5.js
+++ aulasUEG2/tarefa5.js
@@ -1,102 +1,126 @@
 const prompt = require("prompt-sync")();
 
+/////////////////////////////////////////////////////////
+// QUESTÃO 1
+
 function essaAQ1() {
-  let acumulador = 0; // guarda a soma da série
-  let posicao = 1; // controla se soma (+) ou subtrai (-)
+    let acumulador = 0;
+    let posicao = 1;
 
-  for (let denominador = 1; 4 / denominador >= 0.0001; denominador += 2) {
-    let termo = 4 / denominador;
+    for (let denominador = 1; 4 / denominador >= 0.0001; denominador += 2) {
 
-    if (posicao % 2 != 0) {
-      acumulador = acumulador + termo;
-    } else {
-      acumulador = acumulador - termo;
-    } //decide se soma ou subtrai
+        let termo = 4 / denominador;
 
-    posicao++;
-  }
+        if (posicao % 2 != 0) {
+            acumulador = acumulador + termo;
+        } else {
+            acumulador = acumulador - termo;
+        }
 
-  console.log(acumulador);
+        posicao++;
+    }
+
+    console.log("Valor aproximado de PI:", acumulador);
 }
 
 essaAQ1();
+
 /////////////////////////////////////////////////////////
+// QUESTÃO 2
 
 function essaAQ2() {
-  let s = 0;
-  let denominador = 1;
-  let posicao = 1;
 
-  for (let i = 1; i <= 1000; i++) {
-    let termo = 1 / denominador ** 3;
+    let s = 0;
+    let denominador = 1;
+    let posicao = 1;
 
-    if (posicao % 2 != 0) {
-      s = s + termo;
-    } else {
-      s = s - termo;
+    for (let i = 1; i <= 1000; i++) {
+
+        let termo = 1 / (denominador ** 3);
+
+        if (posicao % 2 != 0) {
+            s = s + termo;
+        } else {
+            s = s - termo;
+        }
+
+        denominador += 2;
+        posicao++;
     }
 
-    denominador += 2;
-    posicao++;
-  }
+    let pi = Math.cbrt(32 * s);
 
-  let pi = Math.cbrt(32 * s);
-  console.log(pi);
+    console.log("Valor aproximado de PI:", pi);
 }
 
 essaAQ2();
 
 /////////////////////////////////////////////////////////
+// QUESTÃO 3
 
 function essaAQ3() {
-  let x = Number(prompt("Escolha um numero "));
-  let termo = 0;
-  let acumulador = 0;
-  let valorReal = Math.exp(x);
-  let diferenca = Infinity;
 
-  for (let n = 0; diferenca > 0.0001; n++) {
-    let fatorial = 1;
+    let x = Number(prompt("Escolha um numero: "));
 
-    for (let i = 1; i <= n; i++) {
-      fatorial *= i;
+    let acumulador = 0;
+    let valorReal = Math.exp(x);
+    let diferenca = Infinity;
+
+    for (let n = 0; diferenca > 0.0001; n++) {
+
+        let fatorial = 1;
+
+        for (let i = 1; i <= n; i++) {
+            fatorial *= i;
+        }
+
+        let termo = (x ** n) / fatorial;
+
+        acumulador += termo;
+
+        diferenca = Math.abs(valorReal - acumulador);
     }
 
-    termo = (x ** n) / fatorial;
-
-    acumulador += termo;
-
-    diferenca = Math.abs(valorReal - acumulador);
-  }
-
-  console.log(acumulador);
+    console.log("Valor calculado:", acumulador);
+    console.log("Valor real:", valorReal);
 }
 
-/////////////////////////////////////////////////////////
+essaAQ3();
 
-// potência
+/////////////////////////////////////////////////////////
+// FUNÇÕES AUXILIARES
+
 function potencia(x, n) {
+
     let resultado = 1;
+
     for (let i = 0; i < n; i++) {
         resultado = resultado * x;
     }
+
     return resultado;
 }
 
-// fatorial
 function fatorial(n) {
+
     let resultado = 1;
+
     for (let i = 1; i <= n; i++) {
         resultado = resultado * i;
     }
+
     return resultado;
 }
 
-// série do cosseno (20 termos)
+/////////////////////////////////////////////////////////
+// QUESTÃO 4 - COSSENO
+
 function cosSerie(x) {
+
     let soma = 0;
 
     for (let n = 0; n < 20; n++) {
+
         let sinal;
 
         if (n % 2 == 0) {
@@ -106,49 +130,32 @@ function cosSerie(x) {
         }
 
         let termo = sinal * potencia(x, 2 * n) / fatorial(2 * n);
+
         soma = soma + termo;
     }
 
     return soma;
 }
 
-// PROGRAMA PRINCIPAL
-let x = Number(prompt("Digite x (em radianos):"));
+let xCos = Number(prompt("Digite x para o cosseno (radianos): "));
 
-let cosCalc = cosSerie(x);
-let cosReal = Math.cos(x);
-let diferenca = cosCalc - cosReal;
+let cosCalc = cosSerie(xCos);
+let cosReal = Math.cos(xCos);
+let diferencaCos = cosCalc - cosReal;
 
-// saída
 console.log("Cosseno calculado:", cosCalc);
 console.log("Cosseno real:", cosReal);
-console.log("Diferença:", diferenca);
+console.log("Diferença:", diferencaCos);
 
 /////////////////////////////////////////////////////////
+// QUESTÃO 5 - SENO
 
-// potência
-function potencia(x, n) {
-    let resultado = 1;
-    for (let i = 0; i < n; i++) {
-        resultado = resultado * x;
-    }
-    return resultado;
-}
-
-// fatorial
-function fatorial(n) {
-    let resultado = 1;
-    for (let i = 1; i <= n; i++) {
-        resultado = resultado * i;
-    }
-    return resultado;
-}
-
-// série do seno (20 termos)
 function senSerie(x) {
+
     let soma = 0;
 
     for (let n = 0; n < 20; n++) {
+
         let sinal;
 
         if (n % 2 == 0) {
@@ -158,20 +165,19 @@ function senSerie(x) {
         }
 
         let termo = sinal * potencia(x, 2 * n + 1) / fatorial(2 * n + 1);
+
         soma = soma + termo;
     }
 
     return soma;
 }
 
-// PROGRAMA PRINCIPAL
-let x = Number(prompt("Digite x (em radianos):"));
+let xSen = Number(prompt("Digite x para o seno (radianos): "));
 
-let senCalc = senSerie(x);
-let senReal = Math.sin(x);
-let diferenca = senCalc - senReal;
+let senCalc = senSerie(xSen);
+let senReal = Math.sin(xSen);
+let diferencaSen = senCalc - senReal;
 
-// saída
 console.log("Seno calculado:", senCalc);
 console.log("Seno real:", senReal);
-console.log("Diferença:", diferenca);
+console.log("Diferença:", diferencaSen);
--- aulasUEG2/tarefa6.js
+++ aulasUEG2/tarefa6.js
@@ -57,7 +57,7 @@ function calculaRaiz(n1){
     }
 } 
 
-calculaRaiz(n1);*/
+calculaRaiz(n1);
 
 //##############################
 
@@ -65,12 +65,86 @@ calculaRaiz(n1);*/
 let n1 = Number(prompt("Digite um número: "));
 let n2 = Number(prompt("Digite um número: "));
 
-function cansado(n1,n2){
- while (n2 !== 0) {  //enquanto não for = 0
-        let resto = n1 % n2; //a gente cria uma variável chamada resto pega n1 e divide por n2 e oq sobrar 
-        n1 = n2; // n1 tem o mesmo valor de n2
-        n2 = resto; // n2 tem o valor de resto 
+function cansado(n1, n2){
+
+    let valor1 = n1;
+    let valor2 = n2;
+
+    while (n2 !== 0) { //enquanto for diferente de 0 
+        let resto = n1 % n2; 
+        n1 = n2;
+        n2 = resto; //divide, pega o resto, troca os valores, repete, quando chega em 0 o valor em N1 é o mdc 
     }
-    return n1;
+
+    // aqui n1 virou o MDC
+    let mmc = (valor1 * valor2) / n1; //n1=mdc -> mmc = n1*n2/mdc 
+
+    console.log("MMC =", mmc);
+}
+
+cansado(n1, n2);
+
+//##############################
+
+let numero = Number(prompt("Digite um número:"));
+
+let original = numero;
+let invertido = 0;
+
+while (numero > 0) { //enquanto o numero ainda existir
+
+    let resto = numero % 10; // a gente cria uma variável chamada resto que divide numero por 10 (dividir por 10 remove o ultimo N 123 12,3 por ex)
+ 
+    invertido = invertido * 10 + resto; //reordena os N 
+
+    numero = parseInt(numero / 10); //retira o ultimo digito do N 
+}
+
+if (original == invertido) {
+    console.log("É capicua");
+} else {
+    console.log("Não é capicua");
+}
+
+//##################################
+let numero = 1;
+
+while (numero <= 9) {
+
+    console.log("Tabuada do " + numero);
+
+    let soma = 1;
+
+    while (soma <= 10) {
+
+        let calculo = numero + soma;
+
+        console.log(numero + " + " + soma + " = " + calculo);
+
+        soma++; // da direita 
+    }
+
+    console.log("----------------");
+
+    numero++; // numero ++ pra passar pra próx assim que chegar a 9, assim quando 1 chegar a 1+9 passa pra 2+1 
+}*/
+//##################################
+for (let numero = 1; numero <= 1000; numero++) { //pra uma variavel q começa em 1 enquanto for menor q 100 ++
+
+    let soma = 0;
+
+    // procura divisores
+    for (let i = 1; i < numero; i++) { //enquanto I for menor q N; I ++
+
+        if (numero % i === 0) {
+            soma += i; //soma os divisores ex 6 1+2+3
+        }
+
+    }
+
+    // verifica se é perfeito
+    if (soma === numero) {
+        console.log(numero);
+    }
 
 }
